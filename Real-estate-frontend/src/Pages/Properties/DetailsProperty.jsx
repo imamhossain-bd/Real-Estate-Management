ecaf47782e47358c6d66023d22af0524cb745ef9
imamhossain-bd <imamhossain0098@gmail.com>
2025-02-22 01:52:03 +0600

    Update the Property section

---
+++ Real-estate-frontend/src/Pages/Properties/DetailsProperty.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+
+const DetailsProperty = ({ pasAllProperty }) => {
+    const {id, image, address, bathroom, category, name, owner_pic, price, rating, sqft, status, bed} = pasAllProperty;
+    return (
+        <div>
+            <div>
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                    viewport={{ once: true }}>
+                    <div className="w-[24rem] mt-10 rounded-2xl overflow-hidden shadow-lg bg-white">
+                        <div className="relative">
+                            <img className="w-full h-60 object-cover transition-transform duration-500 transform hover:scale-110" src={image} alt="Summer House" />
+                            <span className="absolute top-2 font-semibold left-2 bg-purple-600 text-white text-base px-3 py-1 rounded-full">
+                                {status}
+                            </span>
+                            <button className="absolute top-2 right-6 border text-white px-[14px] font-bold py-[6px] text-2xl rounded-full shadow-md">
+                                ♡
+                            </button>
+                            <div className="absolute right-5 h-12 w-12 top-[11.5rem] z-30 rounded-full border">
+                                <img className="rounded-full" src={owner_pic} alt="owner Pic" />
+                            </div>
+                        </div>
+                        <div className="p-4">
+                            <p className="text-xl font-semibold">{price}</p>
+                            <div className="flex items-center mt-1">
+                                <span className="text-yellow-500 text-sm">★★★★☆</span>
+                                <span className="ml-2 text-gray-600 text-sm">{rating} (28 Reviews)</span>
+                            </div>
+                            <a href="#" className="text-lg font-bold mt-6 hover:text-[#f56200] ">{name}</a>
+                            <p className="text-gray-600 mt-4 text-sm">
+                                📍 {address}
+                            </p>
+                            <div className="flex items-center justify-between bg-gray-100 p-2 mt-5 rounded-lg text-sm text-gray-600">
+                                <span>🛏 {bed} Bed</span>
+                                <span>🛁 {bathroom} Bathroom</span>
+                                <span>📏 {sqft} Sqft</span>
+                            </div>
+                            <div className="flex justify-between text-sm mb-5 mt-5 text-gray-500">
+                                <span>Listed on: 13 Jan 2023</span>
+                                <span>Category: {category}</span>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+    );
+};
+
+export default DetailsProperty;
--- Real-estate-frontend/src/Pages/Properties/Properties.jsx
+++ Real-estate-frontend/src/Pages/Properties/Properties.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { Range } from "react-range";
+import DetailsProperty from './DetailsProperty';
+
 
 const Properties = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isPrice, setIsPrice] = useState(false);
 
     const options = [
         "Air Conditioning",
@@ -16,15 +22,31 @@ const Properties = () => {
         "High Class Door",
         "Floor Heating System",
         "Garden Include",
-      ];
+    ];
+
+    const [values, setValues] = useState([0, 1000000]);
+    const min = 0;
+    const max = 1000000;
+
+    const [allProperty, setAllProperty] = useState([]);
+
+    useEffect(() => {
+        const fackData = async () => {
+            const res = await fetch('/FackData.json')
+            const data = await res.json();
+            setAllProperty(data);
+            console.log(data)
+        }
+        fackData();
+    }, [])
 
     return (
         <div>
-            <div className='px-28 py-10'>
+            <div className='px-20 py-10'>
                 <h2 className='text-3xl font-bold'>Property Listing</h2>
                 <div className='flex gap-6 justify-center'>
                     <div>
-                        <div className="max-w-md mx-auto bg-white p-6 shadow-lg rounded-lg">
+                        <div className="max-w-[23rem] mx-auto mt-14 bg-white p-6 shadow-lg rounded-lg">
                             <h2 className="text-xl font-semibold text-gray-900 mb-4">Search Property</h2>
 
                             {/* Search Input */}
@@ -35,34 +57,133 @@ const Properties = () => {
                             />
 
                             {/* Dropdowns */}
-                            {["Property Types", "Property Status", "Price", "Bedrooms", "Bathrooms", "Garage"].map(
-                                (label, index) => (
-                                    <select key={index} className="w-full border p-2 rounded-md mb-3">
-                                        <option>{label}</option>
-                                    </select>
-                                )
-                            )}
+                            <div className="relative w-full">
+                                <select className="p-2 w-full outline-none border rounded-md mb-3 appearance-none pr-10">
+                                    <option placeholder="" value="">Property Types</option>
+                                    <option value="">House</option>
+                                    <option value="">Office</option>
+                                    <option value="">Appartment</option>
+                                    <option value="">Condos</option>
+                                    <option value="">Villa</option>
+                                    <option value="">Small Family</option>
+                                    <option value="">Single Room</option>
+                                </select>
+                                <FontAwesomeIcon icon={faAngleDown} className={`absolute right-3 top-5 transform -translate-y-1/2 text-gray-500 transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
+                            </div>
+
+                            <div className="relative w-full">
+                                <select className="p-2 w-full outline-none border rounded-md mb-3 appearance-none pr-10">
+                                    <option className='' value="">Property Status</option>
+                                    <option value="">For Rent</option>
+                                    <option value="">For Sell</option>
+                                </select>
+                                <FontAwesomeIcon icon={faAngleDown} className={`absolute right-3 top-5 transform -translate-y-1/2 text-gray-500 transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
+                            </div>
+
+                            <div className="relative w-full">
+                                <input className="p-2 w-full outline-none border rounded-md mb-3 pr-10" type="text" placeholder="Location" />
+                                <FontAwesomeIcon icon={faLocationDot} className="absolute right-3 top-5 transform -translate-y-1/2 text-gray-500" />
+                            </div>
+
+                            <div className="relative w-full max-w-md mb-5 mx-auto">
+                                {/* Dropdown Button */}
+                                <button
+                                    onClick={() => setIsPrice(!isPrice)}
+                                    className="w-full flex justify-between items-center border  p-3 rounded-md bg-white text-gray-700 shadow-sm  transition-all">
+                                    Price
+                                    <FontAwesomeIcon icon={faAngleDown} className={`transition-transform ${isPrice ? "rotate-180" : ""}`} />
+                                </button>
+
+                                {/* Dropdown Content - Show Only When isOpen is True */}
+                                {isPrice && (
+                                    <div className="absolute left-0 w-full bg-white p-4 shadow-lg border rounded-md mt-2 z-10">
+                                        {/* Tooltip Arrow */}
+                                        <div className="absolute top-[-10px] left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rotate-45 border-t border-l"></div>
+
+                                        {/* Price Range Display */}
+                                        <div className="flex justify-between text-gray-600 text-sm font-semibold mb-3">
+                                            <span>{values[0].toLocaleString()} $</span>
+                                            <span>{values[1].toLocaleString()} $</span>
+                                        </div>
+
+                                        {/* Custom Range Slider */}
+                                        <Range
+                                            step={1000}
+                                            min={min}
+                                            max={max}
+                                            values={values}
+                                            onChange={(newValues) => setValues(newValues)}
+                                            renderTrack={({ props, children }) => (
+                                                <div {...props} className="h-1 bg-[#9c774f] w-full rounded-md">
+                                                    {children}
+                                                </div>
+                                            )}
+                                            renderThumb={({ props }) => (
+                                                <div {...props} className="w-5 h-5 bg-[#9c774f] rounded-full border-2 border-white shadow-md cursor-pointer" />
+                                            )}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+
+                            <div className="relative w-full">
+                                {/* Dropdown Select */}
+                                <select className="p-2 w-full outline-none border rounded-md mb-3 appearance-none pr-10">
+                                    <option value="">Bedrooms</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                </select>
+
+                                {/* Custom Dropdown Arrow */}
+                                <FontAwesomeIcon icon={faAngleDown} className={`absolute right-3 top-5 transform -translate-y-1/2 text-gray-500 transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
+                            </div>
+
+                            <div className="relative w-full">
+                                {/* Dropdown Select */}
+                                <select className="p-2 w-full outline-none border rounded-md mb-3 appearance-none pr-10">
+                                    <option value="">Bathrooms</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                </select>
+
+                                {/* Custom Dropdown Arrow */}
+                                <FontAwesomeIcon icon={faAngleDown} className={`absolute right-3 top-5 transform -translate-y-1/2 text-gray-500 transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
+                            </div>
+
+
+                            <div className="relative w-full">
+                                {/* Dropdown Select */}
+                                <select className="p-2 w-full outline-none border rounded-md mb-3 appearance-none pr-10">
+                                    <option value="">Garage</option>
+                                    <option value="1">Yes</option>
+                                    <option value="2">No</option>
+                                </select>
+
+                                {/* Custom Dropdown Arrow */}
+                                <FontAwesomeIcon icon={faAngleDown} className={`absolute right-3 top-5 transform -translate-y-1/2 text-gray-500 transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
+                            </div>
 
                             {/* Min & Max Area */}
                             <div className="flex space-x-2 mb-3">
-                                <input
-                                    type="text"
-                                    placeholder="Min Area"
-                                    className="w-1/2 border p-2 rounded-md"
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Max Area"
-                                    className="w-1/2 border p-2 rounded-md"
-                                />
+                                <input type="text" placeholder="Min Area" className="w-1/2 outline-none border p-2 rounded-md" />
+                                <input type="text" placeholder="Max Area" className="w-1/2 outline-none border p-2 rounded-md" />
                             </div>
 
-                            
+
                             {/* Advanced Button */}
                             <button
                                 onClick={() => setIsOpen(!isOpen)}
-                                className="w-full border p-2 rounded-md flex items-center justify-between"
-                            >
+                                className="w-full border p-2 rounded-md flex items-center justify-between">
                                 <span>Advanced</span>
                                 <span className="text-xl">⋮</span>
                             </button>
@@ -72,8 +193,7 @@ const Properties = () => {
                                 initial={{ height: 0, opacity: 0 }}
                                 animate={{ height: isOpen ? "auto" : 0, opacity: isOpen ? 1 : 0 }}
                                 transition={{ duration: 0.3, ease: "easeInOut" }}
-                                className="overflow-hidden"
-                            >
+                                className="overflow-hidden">
                                 <h3 className="text-lg font-semibold mt-4">Additional Options</h3>
                                 <div className="mt-2 space-y-2">
                                     {options.map((option, index) => (
@@ -92,7 +212,11 @@ const Properties = () => {
                         </div>
                     </div>
                     <div>
-                        
+                        <div className='grid grid-cols-3 gap-x-10'>
+                            {
+                                allProperty.map((allPro) => <DetailsProperty key={allPro.id} pasAllProperty={allPro} ></DetailsProperty>)
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
